a579b4e57b7c230452c12abd971bac503de040ed
Ustat-Kaur <81715255+Ustat-Kaur@users.noreply.github.com>
2022-05-27 11:34:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -42,4 +42,81 @@ quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance",
 "van","vase","violin","washing machine","watermelon","waterslide","whale","wheel",
 "windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"]
 
-random_no = Math.floor((Math.random()*quick_draw_data_set)+1); 
+random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1); 
+sketch = quick_draw_data_set[random_no];
+console.log(sketch);
+document.getElementById("draw_sketch").innerHTML = "Sketch to be Drawn: " + sketch;
+
+timer_check = "";
+timer_counter = 0;
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
+
+
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("white");
+
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function updateCanvas(){
+    background("white");
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1); 
+    sketch = quick_draw_data_set[random_no];
+    console.log(sketch);
+    document.getElementById("draw_sketch").innerHTML = "Sketch to be Drawn: " + sketch; 
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "timer: " + timer_counter;
+    if (timer_counter > 800){
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if(timer_check == "completed" || answer_holder == "set" ){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch();
+    if(drawn_sketch == sketch) {
+        answer_holder = "set";
+        score++;
+        document.getElementById("score").innerHTML = "score" + score;
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        drawn_sketch = results[0].label;
+        document.getElementById("your_sketch").innerHTML = "your sketch: " + results[0].label;
+        document.getElementById("confidence").innerHTML = "confidence: " + Math.round(results[0].confidence*100) + "%";
+    }
+}
